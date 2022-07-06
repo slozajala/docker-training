dbff59e830f8903c44efcee220081896627f1272
Samuel Loza <samuel.loza@jalasoft.com>
2022-07-05 23:35:51 -0400

    Add volume

--- task1.sh
+++ task1.sh
@@ -9,6 +9,8 @@ docker run --name postgres-db \
 -e POSTGRES_PASSWORD=mySuperPassword \
 -e POSTGRES_USER=sonar-user \
 -e POSTGRES_DB=sonar-db \
+-e PGDATA=/var/lib/postgresql/data/pgdata \
+-v $(pwd)/store/database:/var/lib/postgresql/data \
 -d postgres:latest
 
 # Create the sonarcube
@@ -16,4 +18,8 @@ docker run --name sonarqube \
 --network=task1-net -p 9000:9000 \
 -e SONARQUBE_JDBC_USERNAME=sonar-user \
 -e SONARQUBE_JDBC_PASSWORD=mySuperPassword \
--e SONARQUBE_JDBC_URL=jdbc:postgresql://postgres-db/sonar-db sonarqube:latest
+-e SONARQUBE_JDBC_URL=jdbc:postgresql://postgres-db/sonar-db \
+-v $(pwd)/store/sonarqube/data:/opt/sonarqube/data \
+-v $(pwd)/store/sonarqube/logs:/opt/sonarqube/logs \
+-v $(pwd)/store/sonarqube/extensions:/opt/sonarqube/extensions \
+-d sonarqube:latest
