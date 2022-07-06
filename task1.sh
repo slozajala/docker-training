fe98e6d74638beeb4459f67e61da4db0e985b7da
Samuel Loza <samuel.loza@jalasoft.com>
2022-07-05 23:16:49 -0400

    Add sonarqube

--- task1.sh
+++ task1.sh
@@ -11,3 +11,9 @@ docker run --name postgres-db \
 -e POSTGRES_DB=sonar-db \
 -d postgres:latest
 
+# Create the sonarcube
+docker run --name sonarqube \
+--network=task1-net -p 9000:9000 \
+-e SONARQUBE_JDBC_USERNAME=sonar-user \
+-e SONARQUBE_JDBC_PASSWORD=mySuperPassword \
+-e SONARQUBE_JDBC_URL=jdbc:postgresql://postgres-db/sonar-db sonarqube:latest
