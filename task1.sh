7264e16956cfffb9bab8f16a60ab61d291ceff2d
Samuel Loza <samuel.loza@jalasoft.com>
2022-07-05 23:16:37 -0400

    Add postgres database

--- task1.sh
+++ task1.sh
@@ -2,3 +2,12 @@
 
 # Create the network
 docker network create task1-net
+
+# Create the database
+docker run --name postgres-db \
+--network task1-net \
+-e POSTGRES_PASSWORD=mySuperPassword \
+-e POSTGRES_USER=sonar-user \
+-e POSTGRES_DB=sonar-db \
+-d postgres:latest
+
